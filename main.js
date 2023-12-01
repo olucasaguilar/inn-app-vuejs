a177006b38d7190fd662b57c5970e119fd481aca
Lucas Aguilar <lucasaguilar835@gmail.com>
2023-12-01 02:58:21 -0300

    Tela de detalhes de uma pousada com detalhes dos quartos

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const app = Vue.createApp({
     data(){
         return{
+            currentPage : 'home',            
             searchText: '',
-            listInns: []
+            listInns: [],
+            inn: this.defaultInn()
         }
     },
 
@@ -19,18 +21,19 @@ const app = Vue.createApp({
     },
 
     async mounted() {
-        await this.getData();
+        await this.getDataInns();
     },
 
 
     methods:{
-        async getData() {
+        async getDataInns() {
             let response = await fetch('http://localhost:3000/api/v1/inns');
             let data = await response.json();
 
             data.forEach(item => {                
                 var inn = new Object();
 
+                inn.id = item.id;
                 inn.name = item.name;
                 inn.social_name = item.social_name;
                 inn.cnpj = item.cnpj;
@@ -40,6 +43,55 @@ const app = Vue.createApp({
             
                 this.listInns.push(inn);
             });
+        },
+
+        async getInnDetails(id){
+            let response = await fetch(`http://localhost:3000/api/v1/inns/${id}`);
+            let data = await response.json();
+            this.inn = data;
+        },
+
+        async getInnRooms(id){
+            let response = await fetch(`http://localhost:3000/api/v1/inns/${id}/rooms`);
+            let data = await response.json();
+            this.inn.rooms = data;
+        },
+
+
+        showInnDetails(index){
+            this.getInnDetails(index);
+            this.getInnRooms(index);
+            this.currentPage = 'details';
+        },
+
+        showInnList(){
+            this.inn = this.defaultInn();
+            this.currentPage = 'home';
+        },
+
+        defaultInn(){
+            inn = {
+                id: '',
+                name: '',
+                phone: '',
+                email: '',
+                status: '',
+                address: {
+                    street: '',
+                    neighborhood: '',
+                    state: '',
+                    city: '',
+                    zip_code: ''
+                },
+                average_rating: '',
+                additionals: {
+                    description: '',
+                    policies: '',
+                    check_in: '',
+                    check_out: '',
+                    pets: ''
+                }
+            }
         }
     }
 })
